998f8454524a49bfad0a9a0e62f703551bb195be
Mash36 <mashiur@live.com>
2017-03-24 20:00:29 +1100

    Starting with MongoDB

--- webapp.js
+++ webapp.js
@@ -1,36 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
+var db;
 
 app.use(express.static('static')); // express.static built-in middleware function in Express.
 //Pass the name of the directory that contains the static assets to the 
 //express.static middleware function to start serving the files directly.
 
-//Create an initial array of bugs in the web server
 
-var bugData = [
-	{id: 1, priority: 'P1', status: 'Open', owner: 'Ravan', title:'App crashes on open'},
-	{id: 2, priority: 'P2', status: 'New', owner: 'Eddie', title:'Misaligned border on panel'}
-];
 
 // in the web server, create an endpoint /api/bugs
 app.get('/api/bugs', function(req,res){
-	//stringify and return the array of bugs in this endpoint
-	//res.status(200).send(JSON.stringify(bugData));
-	res.json(bugData);
+	db.collection("bugs").find().toArray(function(err,docs){
+		res.json(docs);
+		//Modifing the GET API to query the data from the DB
+		//using find() and converting it to an array.
+	});
 });
 
 app.use(bodyParser.json());
 app.post('/api/bugs/', function(req, res) {
 	console.log("Req body:", req.body);
 	var newBug = req.body;
-	newBug.id = bugData.length + 1;
-	bugData.push(newBug);
-	res.json(newBug);
+	
+	db.collection("bugs").insertOne(newBug, function(err, result){
+		var newId = result.insertedId;
+		db.collection("bugs").find({_id:newId}).next(function(err,doc){
+			res.json(doc);
+		});
+	});
 });
 
-var server = app.listen(3000, function() {
-	var port = server.address().port;
-	console.log("Started server at port", port);
-});
+MongoClient.connect('mongodb://localhost/bugsdb', function(err, dbConnection){
+	db = dbConnection;
+	var server = app.listen(3000, function() {
+		var port = server.address().port;
+		console.log("Started server at port", port);
+	});
+})
